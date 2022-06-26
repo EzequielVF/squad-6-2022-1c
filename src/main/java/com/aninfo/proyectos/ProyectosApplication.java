efadd3c010a511494ba3ecd58fc3afb590115293
ferbalmaceda23 <fbalmaceda@fi.uba.ar>
2022-06-26 16:52:25 -0300

    agregado de swagger2 para documentacion de api

--- src/main/java/com/aninfo/proyectos/ProyectosApplication.java
+++ src/main/java/com/aninfo/proyectos/ProyectosApplication.java
@@ -2,9 +2,16 @@ package com.aninfo.proyectos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 @Controller
 public class ProyectosApplication {
@@ -14,7 +21,16 @@ public class ProyectosApplication {
 	}
 
 	@RequestMapping("/")
-	public String index(){
+	public String index() {
 		return "html/index.html";
 	}
+
+	@Bean
+	public Docket apiDocket() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+	}
+	//http://localhost:8080/swagger-ui.html#/
 }
